3ac85aabd0eaf9f415ed1463a75a5c279e51456b
BRUNA LUIZA ALVES REAL <u16162@academico.cotuca.unicamp.br>
2018-10-25 09:26:52 -0300

    comentarios no código

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const alunos = [
+const alunos = [ 
     "Maria",
     "João"
 ];
@@ -22,12 +22,13 @@ function completarTabela() {
       umDia.pera = parseInt(Math.random() * 4); // sorteia numeros entre 0 e 4, onde o 4 não está incluso.
       umDia.maca = parseInt(Math.random() * 4);
       umDia.aluno = parseInt(Math.random() * 2);
-      let j = 0; // variavel que axiliará no preenchimento de frutas que a professora levará para casa
       if (umDia.aluno == 0) { //maria foi a melhor aluna
         let qtdM = umDia.pera,
             qtdJ = umDia.maca;
 
         let n = 0;
+
+//Preenche o vetor de frutas que a professora levou para casa
         for (; n < qtdM; n++)
             umDia.frutas[n] = 0;
 
@@ -53,16 +54,17 @@ function completarTabela() {
 //Preenchimento da tabela que será visivel no HTML   
 function preencheTabela() {
     for (let i = 0; i < 30; i++) {
-      let linhaAtual = document.createElement("tr");
+      let linhaAtual = document.createElement("tr"); // cria uma tag tr
       linhaAtual.appendChild(document.createElement("td")).innerText = i + 1; // i começa em zero, então para não aparecer dia 0 somamos 1 no valor de i
-      linhaAtual.appendChild(document.createElement("td")).innerText = mes[i].pera;
+      linhaAtual.appendChild(document.createElement("td")).innerText = mes[i].pera; // inclui dentro da tag tr, uma tag td
       linhaAtual.appendChild(document.createElement("td")).innerText = mes[i].maca;
       linhaAtual.appendChild(document.createElement("td")).innerText = alunos[mes[i].aluno];
       linhaAtual.appendChild(document.createElement("td")).innerText = frutas[mes[i].frutas[0]];
       linhaAtual.appendChild(document.createElement("td")).innerText = frutas[mes[i].frutas[1]];
       linhaAtual.appendChild(document.createElement("td")).innerText = frutas[mes[i].frutas[2]];
 
-      document.getElementById("Info").appendChild(linhaAtual);
+      document.getElementById("Info").appendChild(linhaAtual); // inclui no elemento que possui ID = Info, a linha atual
+                                                               // da tabela.    
     }
    }
    
@@ -72,18 +74,19 @@ function qtdFrutas(){
     let qtdPera = 0,
         qtdMaca = 0;
 
-    for(let i = 0; i < 30; i++)
+    for(let i = 0; i < 30; i++) // percorre os elementos do vetor mes
         for(let n = 0; n < 3; n++)
-            if(mes[i].frutas[n] == 0)
+            if(mes[i].frutas[n] == 0) // percorre os elementos do vetor frutas de cada objeto dia
                 qtdPera++;
             else if(mes[i].frutas[n] == 1)
                 qtdMaca++;
         
-    
-    document.getElementById("peras").value = qtdPera;
+// atribui o valor da variavel ao input do respectivo ID.
+    document.getElementById("peras").value = qtdPera; 
     document.getElementById("macas").value = qtdMaca;
 }
 
+// chamada das funções
 completarTabela();
 preencheTabela();
 qtdFrutas();
